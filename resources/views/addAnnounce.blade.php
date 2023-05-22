d967258f90e4e3abc6584d1b7ef09a03247a9783
Roberto Morreale <roberto_morreale@hotmail.com>
2023-05-22 20:54:55 +0200

    modificata vista create announcement

--- resources/views/addAnnounce.blade.php
+++ resources/views/addAnnounce.blade.php
@@ -1,4 +1,13 @@
 <x-layout title="inserisci annuncio">
 
-
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+    
+                    @livewire('create-announcement')
+    
+                </div>
+            </div>
+        </div>
+    
 </x-layout>
