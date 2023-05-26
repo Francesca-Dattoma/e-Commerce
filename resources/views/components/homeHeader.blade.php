913711b5d0768182bc5685a698dea9dbd37a0439
Fabio Costa <fabiocosta.genova@gmail.com>
2023-05-26 16:34:35 +0200

    impostato componente per header homepage

---
+++ resources/views/components/homeHeader.blade.php
@@ -0,0 +1,18 @@
+<div class="bodyHeaderHome container-fluid">
+    
+    <p id='head1' class='homeHeader text-white monoton-font'>YOeS</p>
+    <p id='head2' class='homeHeader text-white anton-font'>Your</p>
+    <p id='head3' class='homeHeader text-white anton-font'>Own</p>
+    <p id='head4' class='homeHeader text-white anton-font'>eShop</p>
+    <p id='head5' class='homeHeader text-white monoton-font'>YOeS</p>
+    <button class="buttonHomeHeader">Continue</button>
+      <div class='light x1'></div>
+      <div class='light x2'></div>
+      <div class='light x3'></div>
+      <div class='light x4'></div>
+      <div class='light x5'></div>
+      <div class='light x6'></div>
+      <div class='light x7'></div>
+      <div class='light x8'></div>
+      <div class='light x9'></div>
+</div>
