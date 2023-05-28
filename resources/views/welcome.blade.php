23caf4702e6f6740847bb6f2d8e14beeaf3bf865
Gaetano Cerbone <gaetanocerbone92@gmail.com>
2023-05-28 11:40:33 +0200

    sistemato swiper

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,7 @@
     @if($adds)
     
         <h2 class="anton-font display-4 ms-5">Gli ultimi annunci inseriti:</h2>
-        <div class="my-5 ">
+        <div class="my-5">
             <div class="swiper mySwiper rounded shadow container welcomeMain">
                 <div class="swiper-wrapper">
                     @foreach($adds as $add)
