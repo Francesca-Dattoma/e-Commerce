b63a4a6082c4fd379b5633a9a0887657b0a3a501
Fabio Costa <fabiocosta.genova@gmail.com>
2023-06-04 14:26:04 +0200

    fatto frontend emails, aggiunta mail accettazione revisore e aggiunti social

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -55,5 +55,18 @@
         </ul>
       </div>
     </div>
+    <div class="row justify-content-center">
+      
+      <h5 class="text-center" >Seguici sui nostri social:</h5>
+      
+      <div class="col-10 d-flex justify-content-evenly bg-white rounded badged shadow">
+          <tr>
+            <td align="center" valign="top" class="td"><a target="_blank" href="https://facebook.com" class="anchorSocial"><img src="https://xbkomh.stripocdn.email/content/assets/img/social-icons/logo-colored/facebook-logo-colored.png" alt="Fb" title="Facebook" height="45" width="45" class="imgSocial"></a></td>
+            <td align="center" valign="top" class="td"><a target="_blank" href="https://twitter.com/?lang=it" class="anchorSocial"><img src="https://xbkomh.stripocdn.email/content/assets/img/social-icons/logo-colored/twitter-logo-colored.png" alt="Tw" title="Twitter" height="45" width="45" class="imgSocial"></a></td>
+            <td align="center" valign="top" class="td"><a target="_blank" href="https://www.instagram.com/" class="anchorSocial"><img src="https://xbkomh.stripocdn.email/content/assets/img/social-icons/logo-colored/instagram-logo-colored.png" alt="Ig" title="Instagram" height="45" width="45" class="imgSocial"></a></td>
+            <td align="center" valign="top" class="td-linkedin"><a target="_blank" href="https://www.linkedin.com/" class="anchorSocial"><img src="https://xbkomh.stripocdn.email/content/assets/img/social-icons/logo-colored/linkedin-logo-colored.png" alt="In" title="Linkedin" height="45" width="45" class="imgSocial"></a></td>
+          </tr>
+      </div>
+    </div>
   </div>
   </footer>
